8c0137e47e2582c914bd7ba90dca12f1f5ce5400
Jerome Leclanche <jerome@leclan.ch>
2011-08-26 02:19:50 +0100

    Simplify some argument logic

--- main.py
+++ main.py
@@ -25,36 +25,36 @@ def price(value):
 
 class WDBCClient(QApplication):
 	name = "WDBC Reader"
-	
-	def __init__(self, argv):
-		super(WDBCClient, self).__init__(argv)
-		
+
+	def __init__(self, args):
+		super(WDBCClient, self).__init__(args)
+
 		QTextCodec.setCodecForCStrings(QTextCodec.codecForName("UTF-8"))
-		
+
 		self.mainWindow = MainWindow()
 		self.mainWindow.setWindowTitle(self.name)
 		self.mainWindow.resize(1024, 768)
 		self.mainWindow.setMinimumSize(640, 480)
-		
+
 		arguments = ArgumentParser(prog="wdbc")
 		arguments.add_argument("-b", "--build", type=int, dest="build", default=0)
 		arguments.add_argument("--get", action="store_true", dest="get", help="get from the environment")
 		arguments.add_argument("files", nargs="*")
-		args = arguments.parse_args(argv[1:])
+		args = arguments.parse_args(args)
 		self.defaultBuild = args.build
-		
+
 		self.mainWindow.statusBar().showMessage("Ready")
-		
+
 		for name in args.files:
 			if args.get:
 				self.openByGet(name)
 			else:
 				self.open(name)
-	
+
 	def openByGet(self, path):
 		self.mainWindow.addTab(wdbc.get(path, self.defaultBuild))
 		self.mainWindow.setWindowTitle("%s - %s" % (path, self.name))
-	
+
 	def open(self, path):
 		self.mainWindow.addTab(wdbc.open(path, self.defaultBuild))
 		self.mainWindow.setWindowTitle("%s - %s" % (path, self.name))
@@ -63,17 +63,17 @@ class WDBCClient(QApplication):
 class MainWindow(QMainWindow):
 	def __init__(self, *args):
 		super(MainWindow, self).__init__(*args)
-		
+
 		self.__addMenus()
 		self.__addToolbar()
-		
+
 		self.tabWidget = QTabWidget()
 		self.tabWidget.setDocumentMode(True)
 		self.tabWidget.setMovable(True)
 		self.tabWidget.setTabsClosable(True)
 		self.tabWidget.tabCloseRequested.connect(self.actionCloseTab)
 		self.setCentralWidget(self.tabWidget)
-	
+
 	def __addMenus(self):
 		def closeOrExit():
 			index = self.tabWidget.currentIndex()
@@ -81,7 +81,7 @@ class MainWindow(QMainWindow):
 				self.close()
 			else:
 				self.actionCloseTab(index)
-		
+
 		fileMenu = self.menuBar().addMenu("&File")
 		fileMenu.addAction(QIcon.fromTheme("document-open"), "&Open...", self.actionOpen, "Ctrl+O")
 		fileMenu.addAction("Change &build", self.actionChangeBuild, "Ctrl+B")
@@ -90,29 +90,29 @@ class MainWindow(QMainWindow):
 		fileMenu.addAction(QIcon.fromTheme("window-close"), "&Close", closeOrExit, "Ctrl+W")
 		fileMenu.addSeparator()
 		fileMenu.addAction(QIcon.fromTheme("application-exit"), "&Quit", self.close, "Ctrl+Q")
-		
+
 		helpMenu = self.menuBar().addMenu("&Help")
 		helpMenu.addAction(QIcon.fromTheme("help-about"), "About")
-	
+
 	def __addToolbar(self):
 		toolbar = self.addToolBar("Toolbar")
 		toolbar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
 		toolbar.addAction(QIcon.fromTheme("document-open"), "Open").triggered.connect(self.actionOpen)
 		toolbar.addAction(QIcon.fromTheme("x-office-spreadsheet"), "Export").triggered.connect(self.actionExportData)
-	
+
 	def actionChangeBuild(self):
 		file = self.currentModel().file
 		current = file.build
 		build, ok = QInputDialog.getInt(self, "Change build", "Build number", value=current, minValue=-1)
 		if ok and build != current:
 			self.currentModel().setFile(wdbc.open(file.file.name, build))
-	
+
 	def actionCloseTab(self, index):
 		widget = self.tabWidget.widget(index)
 		del widget.model().file
 		del widget
 		self.tabWidget.removeTab(index)
-	
+
 	def actionExportData(self):
 		basename, _ = os.path.splitext(self.currentModel().file.file.name)
 		filename, ok = QInputDialog.getText(self, "Choose a filename", "File name to export to", QLineEdit.Normal, "%s.csv" % (basename))
@@ -123,13 +123,13 @@ class MainWindow(QMainWindow):
 				for row in self.currentModel().itemData:
 					f.write(",".join(str(x) for x in row))
 					f.write("\n")
-	
+
 	def actionOpen(self):
 		filename, filters = QFileDialog.getOpenFileName(self, "Open file", "/var/www/sigrie/caches", "DBC/Cache files (*.dbc *.wdb *.db2 *.dba *.wcf)")
 		if filename:
 			file = wdbc.open(filename)
 			self.addTab(file)
-	
+
 	def addTab(self, file):
 		view = TableView()
 		model = TableModel()
@@ -137,7 +137,7 @@ class MainWindow(QMainWindow):
 		view.setModel(model)
 		view._m_model = model # BUG
 		self.tabWidget.addTab(view, QIcon.fromTheme("x-office-spreadsheet"), os.path.basename(file.file.name))
-	
+
 	def currentModel(self):
 		view = self.tabWidget.currentWidget()
 		return view._m_model # BUG
@@ -156,51 +156,51 @@ class TableModel(QAbstractTableModel):
 		super(TableModel, self).__init__(*args)
 		self.itemData = []
 		self.rootData = []
-	
+
 	def columnCount(self, parent):
 		return len(self.rootData)
-	
+
 	def data(self, index, role):
 		if not index.isValid():
 			return
-		
+
 		if role == Qt.DisplayRole:
 			cell = self.itemData[index.row()][index.column()]
 			field = self.structure[index.column()]
-			
+
 			if isinstance(field, wdbc.structures.HashField) or isinstance(field, wdbc.structures.DataField):
 				cell = hexlify(cell)
-			
+
 			elif isinstance(field, wdbc.structures.BitMaskField):
 				if cell is not None:
 					cell = "0x%08x" % (cell)
-			
+
 			elif isinstance(field, wdbc.structures.MoneyField):
 				gold, silver, copper = price(int(cell))
-				
+
 				gold = gold and "%ig" % (gold)
 				silver = silver and "%is" % (silver)
 				copper = copper and "%ic" % (copper)
-				
+
 				cell = " ".join(x for x in (gold, silver, copper) if x) or "0c"
-			
+
 			# Limit data within cells for performance reasons
 			if isinstance(cell, str) and len(cell) > 200:
 				cell = cell[:200] + "..."
-			
+
 			return cell
-	
+
 	def headerData(self, section, orientation, role):
 		if orientation == Qt.Horizontal and role == Qt.DisplayRole:
 			return self.rootData[section]
-		
+
 		return QAbstractItemModel.headerData(self, section, orientation, role)
-	
+
 	def rowCount(self, parent=QModelIndex()):
 		if parent.isValid():
 			return 0
 		return len(self.itemData)
-	
+
 	def setFile(self, file):
 		self.layoutAboutToBeChanged.emit()
 		self.file = file
@@ -210,7 +210,7 @@ class TableModel(QAbstractTableModel):
 		msg = "%i rows - Using %s build %i" % (self.rowCount(), file.structure, file.build)
 		qApp.mainWindow.statusBar().showMessage(msg)
 		self.layoutChanged.emit()
-	
+
 	def sort(self, column, order=Qt.AscendingOrder):
 		self.layoutAboutToBeChanged.emit()
 		self.itemData = sorted(self.itemData, key=itemgetter(column))
@@ -223,8 +223,8 @@ def main():
 	import signal
 	import sys
 	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	app = WDBCClient(sys.argv)
-	
+	app = WDBCClient(sys.argv[1:])
+
 	app.mainWindow.show()
 	sys.exit(app.exec_())
 
